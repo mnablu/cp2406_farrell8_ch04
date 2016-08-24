35c8c2f6a5c4cedd0714316590049b3062d5c001
mnablu <marc.nablu@my.jcu.edu.au>
2016-08-24 15:48:39 +1000

    Fix Debug 4

---
+++ DebuggingExercises/FixDebugFour1.java
@@ -0,0 +1,16 @@
+// This program assigns values to two variables
+// and performs mathematical operations with them
+public class FixDebugFour1
+{
+   public static void main(String args[])
+   {
+      int x = 5;
+      int y = 8;
+      int z = 19;
+
+      System.out.println("adding x and y is " + (x + y));
+      System.out.println("subtracting x and y is " + (z - y));
+      System.out.println("dividing x and y is " + (z / x));
+      System.out.println("multiplying x and y is " + (x * z));
+   }
+}
